0bb88ff8fefd115fdafefc00c085e6434d37a482
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-05-13 09:55:27 -0400

    Added thresholding

--- ClusterCountPlugin.py
+++ ClusterCountPlugin.py
@@ -1,14 +1,24 @@
 
 class ClusterCountPlugin:
    def input(self, filename):
-      self.myfile = filename
+      parameterfile = open(filename, 'r')
+      self.parameters = dict()
+      for line in parameterfile:
+         contents = line.split('\t')
+         self.parameters[contents[0]] = contents[1].strip()
 
    def run(self):
-      clusterfile = open(self.myfile, 'r')
+      clusterfile = open(self.parameters['inputfile'], 'r')
+      self.threshold = int(self.parameters['threshold'])
       self.count = 0
+      members = 0
       for line in clusterfile:
          if (line.strip() == '\"\",\"x\"'):
-            self.count += 1
+            if (members >= self.threshold):
+               self.count += 1
+               members = 0
+         else:
+            members += 1
 
    def output(self, filename):
-      print("NUMBER OF CLUSTERS ("+self.myfile+"): "+str(self.count))
+      print("NUMBER OF CLUSTERS ("+self.parameters['inputfile']+", THRESHOLD="+str(self.threshold)+"): "+str(self.count))
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # ClusterCount
 # Language: Python
-# Input: CSV (clusters)
+# Input: TXT
 # Output: none (screen, number of clusters)
 # Tested with: PluMA 1.0, Python 3.6
 
 PluMA plugin that takes a CSV file of clusters as input
-and outputs the number of clusters.
+and outputs the number of clusters that are at least a certain
+number of nodes (the 'threshold').
+
+Each of these are specified in an input TXT file of tab-delimited
+keyword, value pairs - with keywords "inputfile" and "threshold".
 
 Clusters are assumed to be delineated by "","x".
